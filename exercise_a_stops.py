5df4cecd1cbcd6e7dd3faff39c4966a87b1c348a
James <james.m.linklater@gmail.com>
2022-06-21 16:33:04 +0100

    changed a5 solution

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -13,11 +13,7 @@ for stop in stops:
         print(count)
     count += 1
 #5. Remove "Livingston" from the list using its name
-num = 0
-for stop in stops:
-    if stop == "Livingston":
-        stops.remove("Livingston")
-    num += 1
+stops.remove("Livingston")
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
 #8. Sort the list alphabetically
