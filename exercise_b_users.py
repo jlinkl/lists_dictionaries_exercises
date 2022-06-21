75bd3bab8a702ed9dcc23f6d8139c5543cbcccd9
James <james.m.linklater@gmail.com>
2022-06-21 17:03:50 +0100

    finished b8

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -77,5 +77,7 @@ print(evens)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"]["lottery_numbers"].append(7)
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
 # 9. Add a pet dog to Erik called "fluffy"
+
 # 10. Add another person to the users dictionary
