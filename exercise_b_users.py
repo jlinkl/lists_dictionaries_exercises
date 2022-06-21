e5c7d834e94c18a72c25ba685154c9aaee06af57
James <james.m.linklater@gmail.com>
2022-06-21 17:00:29 +0100

    finished b6

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -66,6 +66,14 @@ print(f'{users["Avril"]["pets"][0]["species"]}')
 sorts = sorted(users["Erik"]["lottery_numbers"])
 print(sorts[0])
 # 6. Return an list of Avril's lottery numbers that are even
+lists = users["Avril"]["lottery_numbers"]
+evens = []
+count = 0
+for list in lists:
+  if (list % 2) == 0:
+    evens.append(lists[count])
+  count += 1
+print(evens)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
